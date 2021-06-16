bee1d661d5b9394379d0f263576b298fdf51079e
lulimobile <lulimobile@web.de>
2021-06-16 12:47:06 +0200

    16.6

--- FRAGEPOOL.java
+++ FRAGEPOOL.java
@@ -28,7 +28,7 @@ public  class FRAGEPOOL
 
  class RandomNumbers{
         public static void main(String[] args) {
-        	Random objGenerator = new Random();
+            Random objGenerator = new Random();
             for (int iCount = 0; iCount< 10; iCount++){
               int randomNumber = objGenerator.nextInt(100);
               System.out.println("Random No : " + randomNumber); 
