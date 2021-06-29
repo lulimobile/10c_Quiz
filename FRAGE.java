5dc97a790f653f9433341cd2fb419cf51864eea2
lulimobile <lulimobile@web.de>
2021-06-29 11:40:42 +0200

    Neue Klasse

---
+++ FRAGE.java
@@ -0,0 +1,12 @@
+
+
+
+
+ class FRAGE
+{
+    Antwort antwort1;
+    Antwort antwort2;
+    Antwort antwort3;
+    Antwort AntwortRichtig;
+    
+    public class FRAGE(
---
+++ Spiel.java
@@ -0,0 +1,152 @@
+
+public class Spiel 
+{
+    FRAGE frage1;
+    FRAGE frage2;
+    FRAGE frage3;
+    FRAGE frage4;
+    FRAGE frage5;
+    FRAGE frage6;
+    FRAGE frage7;
+    FRAGE frage8;
+    FRAGE frage9;
+    FRAGE frage10;
+    FRAGE frage11;
+    FRAGE frage12;
+    FRAGE frage13;
+    FRAGE frage14;
+    FRAGE frage15;
+    ANTWORT antwort1;
+    ANTWORT antwort2;
+    ANTWORT antwort3;
+    FRAGETEXT fragetext;
+    
+	public static void main(String[] args) {
+
+		
+		frage1 = new FRAGE
+		String[] frage1 = {
+				"fragetext = Wieviel ist 1 + 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+                frage2 = new FRAGE
+		String[] frage2 = {
+				"fragetext = Wieviel ist 1 + 2?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage3 = new FRAGE
+		String[] frage3 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage4 = new FRAGE
+		String[] frage4 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage5 = new FRAGE
+		String[] frage5 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage6 = new FRAGE
+		String[] frage6 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage7 = new FRAGE
+		String[] frage7 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage8 = new FRAGE
+		String[] frage8 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage9 = new FRAGE
+		String[] frage9 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage10 = new FRAGE
+		String[] frage10 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage11 = new FRAGE
+		String[] frage11 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage12 = new FRAGE
+		String[] frage12 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage13 = new FRAGE
+		String[] frage13 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage14 = new FRAGE 
+		String[] frage14 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		frage15 = new FRAGE
+		String[] frage15 = {
+				"fragetext = Wieviel ist 2 - 1?",
+				"antwort1 = Eins",
+				"antwort2 = Zwei",
+				"antwort3 = Drei",
+				
+		};
+		
+		String[][] liste = {
+				frage1,frage2,frage3,frage4,frage5,frage6,frage7,frage8,frage9,frage10,frage11,frage12,frage13,frage14,frage15
+		};
+}
+}
