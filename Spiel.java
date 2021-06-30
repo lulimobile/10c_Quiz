618f117dabaa27547ae2fd0d832b45294ed1d263
lulimobile <lulimobile@web.de>
2021-06-30 12:10:43 +0200

    zhh

--- Spiel.java
+++ Spiel.java
@@ -21,136 +21,138 @@ public class Spiel
     String antwort3;
     String fragetext;
     FRAGE [] frage;
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		
-		frage1 = new FRAGE[15];
-		for (int i = 1; i < frage. nummer ; i++)
+        
+        frage1 = new FRAGE[15];
+        for (int i = 1; i < frage. nummer ; i++)
         {
             frage [i] = new FRAGE ();
         }
-		 frage1 = 
-				fragetext1 = "Was ist eine Gewichtsklasse beim Boxen?";
-				t = antwort1 = "Halbschergewicht";
-				antwort2 = "Achtelleichtgewicht";
-				antwort3 = "Viertelstrohgewicht";
-				
-		
-                frage2 = new FRAGE
-		[String[] frage2 = 
-				fragetext1 = "Welches ist das seltenste Metall?";
-			        antwort1 = "Platin";
-				antwort2 = "Indium";
-				t = antwort3 = "Promethium";
-				
-		
-		frage3 = new FRAGE
-		[String[] frage3 = 
-				fragetext1 = "Womit bezahlt man in Venezuela?";
-				antwort1 = "Nuevo Sol";
-				t = antwort2 = "Bolivar";
-				antwort3 = "Cordoba";
-				
-		
-		frage4 = new FRAGE
-		[String[] frage4 = 
-				fragetext1 = "Wer war der britische Premierminister 2015?";
-				antwort1 = "Gordon Brown";
-				t = antwort2 = "David Cameron";
-				antwort3 = "John Craydon";
-				
-		
-		frage5 = new FRAGE
-		[String[] frage5 = 
-				fragetext1 = "Welcher der Jünger Jesu wird als der Ungläubige bezeichnet?";
-				antwort1 = "Petrus";
-				t = antwort2 = "Thomas";
-				antwort3 = "Markus";
-				
-		
-		frage6 = new FRAGE
-		[String[] frage6 = 
-				fragetext1 = "Wofür ist Vertigo der medizinische Fachausdruck?";
-				antwort1 = "Taubheit";
-				antwort2 = "Brechreiz";
-				t = antwort3 = "Schwindel";
-				
-		
-		frage7 = new FRAGE
-		[String[] frage7 = 
-				fragetext1 = "Wie heißt der E-Mail-Computervirus, der weltweit Millionen von Computern lahmlegte?";
-				t = antwort1 = "I love you";
-				antwort2 = "I want you";
-				antwort3 = "Strictly confidental";
-				
-		
-		frage8 = new FRAGE
-		[String[] frage8 = 
-				fragetext1 = "Wer besang den Komissar";
-				t = antwort1 = "Falco";
-				antwort2 = "Peter Maffay";
-				antwort3 = "Fanta4";
-				
-		
-		frage9 = new FRAGE
-		[String[] frage9 = 
-				fragetext1 = "Wer besang Summer of '69?";
-				antwort1 = "Sting";
-				antwort2 = "Bon Jovi";
-				t = antwort3 = "Bryan Adams";
-				
-		
-		frage10 = new FRAGE
-		[String[] frage10 = 
-				fragetext1 = "Zwischen wem fand der Hundertjährige Krieg?";
-				antwort1 = "Russland und Polen";
-				t = antwort2 = "England und Frankreich";
-				antwort3 = "Frankreich und Spanien";
-				
-		
-		frage11 = new FRAGE
-		[String[] frage11 = 
-				fragetext = "Mit welchem Klopp gewann Jürgen Klopp 2020 die Premierleague?";
-				antwort1 = "ManU";
-				t = antwort2 = "Liverpool";
-				antwort3 = "Chelsea";
-				
-		
-		frage12 = new FRAGE
-		[String[] frage12 = 
-				fragetext1 ="Was ist ein Tapir ?";
-				antwort1 = "Vogel";
-				t = antwort2 = "Säugetier";
-				antwort3 = "Fisch";
-				
-		
-		frage13 = new FRAGE
-		(String[] frage13 = 
-				fragetext1 = "In welchem Jahr wurde erstmals das olympische Feuer gezündet?",
-				t = antwort1 = "1936";
-				antwort2 = "1944";
-				antwort3 = "1966";
-				
-		
-		frage14 = new FRAGE
-		[String[] frage14 = 
-				fragetext1 = "Wo liegt die Antarktis?";
-				t = antwort1 = "Südpol";
-				antwort2 = "Nordpol";
-				antwort3 = "Äquator";
-				
-		
-		frage15 = new FRAGE
-		[String[] frage15 = 
-				fragetext1 = "Wie hoch war Einsteins IQ?";
-				 antwort1 = "180";
-				antwort2 = "169";
-				t = antwort3 = "160";
-				
-		
-		
-		String[] liste = {
-				frage1,frage2,frage3,frage4,frage5,frage6,frage7,frage8,frage9,frage10,frage11,frage12,frage13,frage14,frage15
-		};
+         frage1 = new FRAGE();
+         String[] frage1 = {
+                fragetext1 = "Was ist eine Gewichtsklasse beim Boxen?";
+                t = antwort1 = "Halbschwergewicht";
+                antwort2 = "Achtelleichtgewicht";
+                antwort3 = "Viertelstrohgewicht";
+               
+        
+                frage2 = new FRAGE(); 
+        String[] frage2 = {
+                fragetext1 = "Welches ist das seltenste Metall?";
+                    antwort1 = "Platin";
+                antwort2 = "Indium";
+                t = antwort3 = "Promethium";
+                
+            
+        frage3 = new FRAGE();
+                String[] frage2 ={
+                    
+                }fragetext1 = "Womit bezahlt man in Venezuela?";
+                antwort1 = "Nuevo Sol";
+                t = antwort2 = "Bolivar";
+                antwort3 = "Cordoba";
+                
+            
+        frage4 = new FRAGE();
+        String[] frage4 = 
+                fragetext1 = "Wer war der britische Premierminister 2015?";
+                antwort1 = "Gordon Brown";
+                t = antwort2 = "David Cameron";
+                antwort3 = "John Craydon";
+                
+        
+        frage5 = new FRAGE();
+        String[] frage5 = 
+                fragetext1 = "Welcher der Jünger Jesu wird als der Ungläubige bezeichnet?";
+                antwort1 = "Petrus";
+                t = antwort2 = "Thomas";
+                antwort3 = "Markus";
+                
+        
+        frage6 = new FRAGE();
+        String[] frage6 = 
+                fragetext1 = "Wofür ist Vertigo der medizinische Fachausdruck?";
+                antwort1 = "Taubheit";
+                antwort2 = "Brechreiz";
+                t = antwort3 = "Schwindel";
+                
+        
+        frage7 = new FRAGE();
+        String[] frage7 = 
+                fragetext1 = "Wie heißt der E-Mail-Computervirus, der weltweit Millionen von Computern lahmlegte?";
+                t = antwort1 = "I love you";
+                antwort2 = "I want you";
+                antwort3 = "Strictly confidental";
+                
+        
+        frage8 = new FRAGE();
+        String[] frage8 = 
+                fragetext1 = "Wer besang den Komissar";
+                t = antwort1 = "Falco";
+                antwort2 = "Peter Maffay";
+                antwort3 = "Fanta4";
+                
+        
+        frage9 = new FRAGE();
+        String[] frage9 = 
+                fragetext1 = "Wer besang Summer of '69?";
+                antwort1 = "Sting";
+                antwort2 = "Bon Jovi";
+                t = antwort3 = "Bryan Adams";
+                
+        
+        frage10 = new FRAGE();
+        String[] frage10 = 
+                fragetext1 = "Zwischen wem fand der Hundertjährige Krieg?";
+                antwort1 = "Russland und Polen";
+                t = antwort2 = "England und Frankreich";
+                antwort3 = "Frankreich und Spanien";
+                
+        
+        frage11 = new FRAGE();
+        String[] frage11 = 
+                fragetext = "Mit welchem Klopp gewann Jürgen Klopp 2020 die Premierleague?";
+                antwort1 = "ManU";
+                t = antwort2 = "Liverpool";
+                antwort3 = "Chelsea";
+                
+        
+        frage12 = new FRAGE();
+        String[] frage12 = 
+                fragetext1 ="Was ist ein Tapir ?";
+                antwort1 = "Vogel";
+                t = antwort2 = "Säugetier";
+                antwort3 = "Fisch";
+                
+        
+        frage13 = new FRAGE();
+        String[] frage13 = 
+                fragetext1 = "In welchem Jahr wurde erstmals das olympische Feuer gezündet?",
+                t = antwort1 = "1936";
+                antwort2 = "1944";
+                antwort3 = "1966";
+                
+        
+        frage14 = new FRAGE();
+        String[] frage14 = 
+                fragetext1 = "Wo liegt die Antarktis?";
+                t = antwort1 = "Südpol";
+                antwort2 = "Nordpol";
+                antwort3 = "Äquator";
+                
+        
+        frage15 = new FRAGE();
+        String[] frage15 = 
+                fragetext1 = "Wie hoch war Einsteins IQ?";
+                 antwort1 = "180";
+                antwort2 = "169";
+                t = antwort3 = "160";
+                
+        
+        
+        String[] liste = {
+                frage1,frage2,frage3,frage4,frage5,frage6,frage7,frage8,frage9,frage10,frage11,frage12,frage13,frage14,frage15
+        };
 }
 }
